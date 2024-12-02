1406b79727a7892080d1222bfeac3e52cf182a96
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2024-12-03 02:40:00 +0530

    solve bucket name conflicts

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "s3" {
-    bucket         = "scalable-infra-bucket-752"
+    bucket         = "scalable-infra-bucket-777"
     dynamodb_table = "state-lock"
     key            = "infra/terraform.tfstate"
     region         = "us-east-1"
